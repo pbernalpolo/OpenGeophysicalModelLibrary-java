81ca7c0f98b7e9bed45c4677bb5f46a4e5a31b7c
pbernalpolo <pablo.bernal.polo@gmail.com>
2026-06-25 16:38:39 +0200

    refactor: create gravityModels package and move classes
    Egm2008 and SphericalHarmonicGravityModel to it.
    
    We also move tests.

--- src/main/java/geophysicalModelLibrary/gravityModels/Egm2008.java
+++ src/main/java/geophysicalModelLibrary/gravityModels/Egm2008.java
@@ -1,4 +1,4 @@
-package geophysicalModelLibrary;
+package geophysicalModelLibrary.gravityModels;
 
 
 import java.io.FileInputStream;
--- src/test/java/geophysicalModelLibrary/gravityModels/Egm2008Test.java
+++ src/test/java/geophysicalModelLibrary/gravityModels/Egm2008Test.java
@@ -1,4 +1,4 @@
-package geophysicalModelLibrary;
+package geophysicalModelLibrary.gravityModels;
 
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
